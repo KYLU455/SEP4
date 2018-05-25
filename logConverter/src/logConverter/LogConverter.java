f8e04cac1d7244a521a98c19bdb3389ade30e8de
KYLU455 <kylu455@gmail.com>
2018-05-25 12:06:16 +0200

    pls stop

--- logConverter/src/logConverter/LogConverter.java
+++ logConverter/src/logConverter/LogConverter.java
@@ -5,6 +5,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.sql.SQLException;
 import java.util.GregorianCalendar;
 
@@ -12,6 +16,9 @@ public class LogConverter {
 
 	private File[] files;
 	private DatabaseCommunication db;
+	private int day;
+	private int month;
+	private int year;
 	
 	public LogConverter() throws IOException {
 		File forder = new File("logs");
@@ -35,17 +42,26 @@ public class LogConverter {
 						line.charAt(24),
 						line.substring(15, 24),
 						line.substring(7, 15),
-						line.substring(1, 3),
-						line.substring(3, 5),
-						line.substring(5, 7));
+						Integer.parseInt(line.substring(1, 3)),
+						Integer.parseInt(line.substring(3, 5)),
+						Integer.parseInt(line.substring(5, 7)));
 				try {
-					db.insertLog(log, file.getName().substring(0, file.getName().length() - 4));
+					db.insertLog(log, file.getName().substring(0, file.getName().length() - 4), day, month, year);
 				} catch (SQLException e) {
 					System.out.println(line);
 					e.printStackTrace();
 				}
 			}
+			else if (line.substring(0, 5).equals("HFDTE")) {
+				day = Integer.parseInt(line.substring(5, 7));
+				month = Integer.parseInt(line.substring(7, 9));
+				year = Integer.parseInt(line.substring(9, 11));
+			}
 		}
+		bufferedReader.close();
+        Path moveto = FileSystems.getDefault().getPath("logs_archive/" + file.getName());
+        Path movefrom = FileSystems.getDefault().getPath(file.getPath());
+        Files.move(movefrom, moveto, StandardCopyOption.ATOMIC_MOVE);
 	}
 	
 
