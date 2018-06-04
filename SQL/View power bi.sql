c6a3a62129c9b589effdf738689e5677cbc153f0
LAPTOP-D8H66V0I\marij <mariajoseferreirafernandes@gmail.com>
2018-06-05 00:17:41 +0200

    Last changes in power bi view
    changed inner joins to left outer joins because we were getting only one result. Now we get all the thermals joines with everything. Also added the cloud_cover_name

--- SQL/View power bi.sql
+++ SQL/View power bi.sql
@@ -4,8 +4,12 @@ create view sep_power_bi as
 select F_THERMAL.VALID_TO, D_FLIGHT.LOG_NAME,
   D_DATE.YEAR, D_DATE.MONTH, D_DATE.day, D_DATE.HOUR, D_DATE.MINUTE, D_DATE.WEEK_NUMBER, D_DATE.DAY_OF_WEEK, D_DATE.SEASON,
   D.START_LATITUDE, D.END_LATITUDE, D.START_LONGITUDE, D.END_LONGITUDE,
-  DEW_POINT_TEMPERATURE, SURFACE_TEMPERATURE, CLOUD_COVER, WIND_DIRECTION, WIN_SPEED_IN_KNOTS, ISSUING_AIRPORT from F_THERMAL
-  inner join D_FLIGHT on F_THERMAL.FLIGHT_ID = D_FLIGHT.ID
-  inner JOIN D_DATE on F_THERMAL.DATE_FOUND_ID = D_DATE.ID
-  inner join D_GRID D on F_THERMAL.GRID_ID = D.ID
-  inner join F_WEATHER on D_DATE.ID = F_WEATHER.DATE_ID;
+  DEW_POINT_TEMPERATURE, SURFACE_TEMPERATURE, CLOUD_COVER, cloud_cover_name, WIND_DIRECTION, WIN_SPEED_IN_KNOTS, ISSUING_AIRPORT from F_THERMAL
+  left outer join D_FLIGHT on F_THERMAL.FLIGHT_ID = D_FLIGHT.ID
+  left outer JOIN D_DATE on F_THERMAL.DATE_FOUND_ID = D_DATE.ID
+  left outer join D_GRID D on F_THERMAL.GRID_ID = D.ID
+  left outer join F_WEATHER on D_DATE.ID = F_WEATHER.DATE_ID;
+  
+  --Check the right amount with the following count the results should be the same.
+  --select count(log_name) from sep_power_bi;
+  --select count(flight_id) from f_thermal;
