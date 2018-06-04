6bde348829ed3714d17b1a620d60b28b4e6d7539
LAPTOP-D8H66V0I\marij <mariajoseferreirafernandes@gmail.com>
2018-06-05 01:19:11 +0200

    fixing view again, not finished

--- SQL/View power bi.sql
+++ SQL/View power bi.sql
@@ -8,8 +8,32 @@ select F_THERMAL.VALID_TO, D_FLIGHT.LOG_NAME,
   left outer join D_FLIGHT on F_THERMAL.FLIGHT_ID = D_FLIGHT.ID
   left outer JOIN D_DATE on F_THERMAL.DATE_FOUND_ID = D_DATE.ID
   left outer join D_GRID D on F_THERMAL.GRID_ID = D.ID
-  left outer join F_WEATHER on D_DATE.ID = F_WEATHER.DATE_ID;
+ --left outer join F_WEATHER on D_DATE.ID = F_WEATHER.DATE_ID;
+left outer join (
+  select DEW_POINT_TEMPERATURE, SURFACE_TEMPERATURE, CLOUD_COVER, cloud_cover_name, WIND_DIRECTION, WIN_SPEED_IN_KNOTS, ISSUING_AIRPORT,
+  D_DATE.YEAR, D_DATE.MONTH, D_DATE.day from
+  f_weather left outer join d_date on D_DATE.ID = F_WEATHER.DATE_ID where d_date.id !=-1) weather on 
+  D_DATE.year = weather.year AND d_date.day=weather.day AND D_DATE.month=weather.month;
+  
   
   --Check the right amount with the following count the results should be the same.
-  --select count(log_name) from sep_power_bi;
-  --select count(flight_id) from f_thermal;
+  select count(log_name) from sep_power_bi;
+  select count(flight_id) from f_thermal;
+  
+  /* testing stuff here
+ select * from(
+  select DEW_POINT_TEMPERATURE, SURFACE_TEMPERATURE, CLOUD_COVER, cloud_cover_name, WIND_DIRECTION, WIN_SPEED_IN_KNOTS, ISSUING_AIRPORT,
+  D_DATE.YEAR, D_DATE.MONTH, D_DATE.day from
+  f_weather left outer join d_date on D_DATE.ID = F_WEATHER.DATE_ID where d_date.id !=-1 and
+   D_DATE.year =2018 AND d_date.day=11 AND D_DATE.month=05) WHERE rownum = 1;
+  
+  
+   select * from(
+  select DEW_POINT_TEMPERATURE, SURFACE_TEMPERATURE, CLOUD_COVER, cloud_cover_name, WIND_DIRECTION, WIN_SPEED_IN_KNOTS, ISSUING_AIRPORT,
+  D_DATE.YEAR, D_DATE.MONTH, D_DATE.day from
+  f_weather left outer join d_date on D_DATE.ID = F_WEATHER.DATE_ID where d_date.id !=-1 and
+   D_DATE.year =2018 AND d_date.day=12 AND D_DATE.month=05) WHERE rownum = 1;
+  
+  */
+  
+  
